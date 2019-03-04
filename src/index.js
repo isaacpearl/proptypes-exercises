dfbe2a22340fdbbbda096f5cb22216fd5b93cd95
isaaacp <pearl.isaac@gmail.com>
2019-03-04 18:43:38 -0500

    envelope component and subcomponents

--- src/index.js
+++ src/index.js
@@ -3,13 +3,56 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import PropTypes from 'prop-types';
 
+
+
+function Envelope({ toPerson, fromPerson}) {
+	return(
+		<div className='envelope'>
+			<div className='from-person'><AddressLabel person={fromPerson}/></div>
+			<div className='to-person'><AddressLabel person={toPerson}/></div>
+			<Stamp/>
+		</div>
+	);
+}
+
 function AddressLabel({ person }) {
 	return(
-		<div className='address-label'>This is the label.</div>
+		<div className='address-label'>
+			<div>{person.name}</div>
+			<div>{person.address.street}</div>
+			<div>{person.address.city}, {person.address.state} {person.address.zipCode}</div>
+		</div>
 	);
+}
+
+function Stamp() {
+	return(
+		<span className='stamp'></span>
+	)
+}
+
+
+var testToPerson = {
+	name: "Good Friend",
+	address: {
+		street: "808 Beet St.",
+		city: "Wendell",
+		state: "MA",
+		zipCode: "00000"
+	}
+}
+
+var testFromPerson = {
+	name: "Old Buddy",
+	address: {
+		street: "909 Dub Ct.",
+		city: "Los Angeles",
+		state: "CA",
+		zipCode: "99999"
+	}
 }
 
 
 ReactDOM.render(
-	<AddressLabel/>,
+	<Envelope toPerson = {testToPerson} fromPerson={testFromPerson}/>,
 	document.querySelector('#root'));
